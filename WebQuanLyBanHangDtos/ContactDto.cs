162172ec80607c3da23fc8b7c4b4ba52118df3aa
Van Cong <nguyencong2806@gmail.com>
2022-07-11 09:54:59 +0700

    Footer

--- WebQuanLyBanHangDtos/ContactDto.cs
+++ WebQuanLyBanHangDtos/ContactDto.cs
@@ -4,6 +4,7 @@ namespace WebQuanLyBanHangDtos
 {
     public class ContactDto
     {
+        public int Stt { get; set; }
         public int ID { get; set; }
         public string Content { get; set; }
         public Nullable<bool> Status { get; set; }
--- WebQuanLyBanHangDtos/FooterDto.cs
+++ WebQuanLyBanHangDtos/FooterDto.cs
@@ -4,6 +4,7 @@ namespace WebQuanLyBanHangDtos
 {
     public class FooterDto
     {
+        public int Stt { get; set; }
         public string ID { get; set; }
         public string Content { get; set; }
         public Nullable<bool> Status { get; set; }
