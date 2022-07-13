095e9c992e495065026705075ea6454e26f94534
congdevnet <vanconggit2806@gmail.com>
2022-07-14 06:37:57 +0700

    Cap nhat code ngay14/07

---
+++ WebBanHang_Common/MD5Pasword.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+namespace WebBanHang_Common
+{
+    public static class MD5Pasword
+    {
+        public static string GetMD5(string input)
+        {
+            using (MD5CryptoServiceProvider MD5 = new MD5CryptoServiceProvider())
+            {
+                byte[] b = System.Text.Encoding.UTF8.GetBytes(input);
+                b = MD5.ComputeHash(b);
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                foreach (byte x in b)
+
+                    sb.Append(x.ToString("x2"));
+
+                return sb.ToString();
+            }
+        }
+    }
+}
